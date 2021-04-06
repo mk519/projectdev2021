c79995c713373eca2cb59535935d59fd088fe58a
Nathan <nathansaccon10@hotmail.com>
2021-04-05 21:49:04 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -107,8 +107,8 @@ function UpdatePageCounts(response) {
     document.getElementById("nameTopScreen").innerHTML = user.firstName + " " + user.lastName;
     document.getElementById("nameTopScreen1").innerHTML = user.firstName + " " + user.lastName;
     document.getElementById("totalCourses").innerHTML = numClasses;
-    document.getElementById("totalAssignments").innerHTML = numExams;
-    document.getElementById("totalExams").innerHTML = numAssignments;
+    document.getElementById("totalAssignments").innerHTML = numAssignments;
+    document.getElementById("totalExams").innerHTML = numExams;
 }
 
 function CreateScheduleRows(response) {
